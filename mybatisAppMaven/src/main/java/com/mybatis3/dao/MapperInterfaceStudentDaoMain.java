e533ec82c960c325e106c9dbf0a230fc286baf92
bongkyoun <kbk6706@gmail.com>
2022-08-25 17:37:22 +0900

    08.25

---
+++ mybatisAppMaven/src/main/java/com/mybatis3/dao/MapperInterfaceStudentDaoMain.java
@@ -0,0 +1,17 @@
+package com.mybatis3.dao;
+
+public class MapperInterfaceStudentDaoMain {
+
+	public static void main(String[] args) {
+		MapperInterfaceStudentDao mapperInterfaceStudentDao=
+				new MapperInterfaceStudentDao();
+		System.out.println("<<<<<<<<<<<<<< StudentMapper Interface를 사용한Dao메쏘드호출 >>>>>>>>>");
+		System.out.println("---------findAllStudents-----------------------------");
+		System.out.println(mapperInterfaceStudentDao.findAllStudents());
+		System.out.println("---------findStudentById-----------------------------");
+		System.out.println(mapperInterfaceStudentDao.findStudentById(3));
+		System.out.println("---------findStudentByIdWithAddress-----------------------------");
+		System.out.println(mapperInterfaceStudentDao.findStudentByIdWithAddress(3));
+	}
+
+}
